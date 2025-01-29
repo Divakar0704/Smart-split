f37e0e6bb2580a70625de5274b1af605b8328da4
Divakar0704 <reddydivakar4832@gmail.com>
2025-01-29 20:03:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("start.ejs");
+  });
+
+  app.get("/login", (req, res) => {
+    res.render("login.ejs");
+  });
+
+  app.get("/register", (req, res) => {
+    res.render("register.ejs");
+  });
+
+  app.get("/about", (req, res) => {
+    res.render("about.ejs");
+  });
+
+  app.get("/planner", (req, res) => {
+    res.render("planner.ejs");
+  });
+
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  }); 
