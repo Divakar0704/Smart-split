4d1da5064418c6bf6a1b90974be9e1390b41f8b9
Divakar0704 <reddydivakar4832@gmail.com>
2025-02-09 14:28:05 +0530

    dirfilepaths

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+import path from "path";
+
+// Convert __dirname manually
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
